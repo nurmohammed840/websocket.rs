fae58002fcc79f1e6e7c939c40870fcbf4438def
Nur <nurmohammed840@gmail.com>
2023-03-17 18:51:48 +0600

    save

--- examples/autobahn.rs
+++ examples/autobahn.rs
@@ -9,8 +9,6 @@ use web_socket::{CloseCode, DataType, Event, Fragment, MessageType, WebSocket, S
 #[tokio::main(flavor = "current_thread")]
 async fn main() {}
 
-// ------------------------------------------------------------------------------------
-
 mod server {
     use super::*;
     use hyper::{
@@ -37,12 +35,22 @@ mod server {
     }
 
     async fn server_upgrade(mut req: Request<Incoming>) -> hyper::Result<Response<String>> {
-        let mut res = Response::builder();
+        let headers = req.headers();
+        let mut responce = Response::builder();
 
-        if !req.headers().contains_key(UPGRADE) {
-            // res.status(StatusCode::BAD_REQUEST).body(String::from("No upgrade request found."));
-            // return res.body("Not".into());
+        let sec_ws_key = headers.get(SEC_WEBSOCKET_KEY);
+        if !headers.contains_key(UPGRADE)
+            || !headers.contains_key(CONNECTION)
+            || sec_ws_key.is_none()
+        {
+            let response = responce
+                .status(StatusCode::BAD_REQUEST)
+                .body(String::from("expected websocket upgrade request"))
+                .unwrap();
+
+            return Ok(response);
         }
+        let ws_accept = utils::handshake::accept_key_from(sec_ws_key.unwrap());
 
         tokio::spawn(async move {
             match upgrade::on(&mut req).await {
@@ -54,20 +62,31 @@ mod server {
                 Err(err) => eprintln!("upgrade error: {err:#?}"),
             }
         });
-
-        let res = res
+        let responce = responce
             .status(StatusCode::SWITCHING_PROTOCOLS)
             .header(UPGRADE, "websocket")
             .header(CONNECTION, "Upgrade")
-            .header(SEC_WEBSOCKET_ACCEPT, "value")
+            .header(SEC_WEBSOCKET_ACCEPT, ws_accept)
             .body(String::new())
             .unwrap();
 
-        Ok(res)
+        Ok(responce)
     }
 }
 
-async fn handle(mut ws: WebSocket<SERVER, hyper::upgrade::Upgraded>) -> Result<()> {
+mod client {
+    use super::*;
+    use hyper::client::conn::http1;
+    use tokio::net::TcpStream;
+
+    async fn connect(addr: &str, path: &str) -> Result<()> {
+        let stream = TcpStream::connect(addr).await?;
+        // let (mut sender, conn) = http1::handshake(stream).await.unwrap();
+        Ok(())
+    }
+}
+
+async fn handle<const SIDE: bool>(mut ws: WebSocket<SIDE, hyper::upgrade::Upgraded>) -> Result<()> {
     let mut buf = Vec::with_capacity(4096);
     let mut msg_ty = MessageType::Text;
     loop {
